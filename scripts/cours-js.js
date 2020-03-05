2f94fe7ac5da7a2659cb1e8c7967294e50d3a42a
emmyxeme <61809388+emmyxeme@users.noreply.github.com>
2020-03-05 01:03:13 +0100

    Add files via upload

---
+++ scripts/cours-js.js
@@ -0,0 +1,15 @@
+let positive = 42
+let negative = -42
+let decimal = 42.42
+
+console.log(positive)
+
+alert(typeof decimal)
+
+let userIsSignedIn= true
+console.log(userIsSignedIn)
+
+let firstName='Jean'
+let lastName='Mouyo'
+
+console.log(typeof firstName + lastName)
